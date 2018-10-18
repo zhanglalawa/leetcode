b6b87d9d1407e1cbc563b11ac745bcec375213cd
Zhanglala <757259884@qq.com>
2018-10-18 22:31:24 +0800

    TwoSumII and KthLargest

--- leetcode/src/array/SortColors75.java
+++ leetcode/src/array/SortColors75.java
@@ -1,34 +1,35 @@
 package array;
 
 import java.util.Arrays;
+import java.util.Random;
+
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 
 public class SortColors75 {
 	/*
-	 * 计数排个序
-	 * 这里用到辅助空间了
-	 * 适用于元素个数非常有限的
+	 * 计数排个序 这里用到辅助空间了 适用于元素个数非常有限的
 	 */
 	class Solution {
-	    public void sortColors(int[] nums) {
-	        int[] number = new int[3];
-	        for(int n : nums){
-	            if(n == 0){
-	                number[0]++;
-	            }else if(n == 1){
-	                number[1]++;
-	            }else{
-	                number[2]++;
-	            }
-	        }
-	        int index = 0;
-	        for(int j = 0; j<3; j++){
-	            for(int i = 0;i < number[j];i ++){
-	                nums[index++] = j;
-	            }
-	        }
-	    }
+		public void sortColors(int[] nums) {
+			int[] number = new int[3];
+			for (int n : nums) {
+				if (n == 0) {
+					number[0]++;
+				} else if (n == 1) {
+					number[1]++;
+				} else {
+					number[2]++;
+				}
+			}
+			int index = 0;
+			for (int j = 0; j < 3; j++) {
+				for (int i = 0; i < number[j]; i++) {
+					nums[index++] = j;
+				}
+			}
+		}
 	}
-	
+
 	/*
 	 * 三路快排的思想在原地就可以完成
 	 */
@@ -38,24 +39,26 @@ public class SortColors75 {
 			nums[i] = nums[j];
 			nums[j] = tmp;
 		}
-	    public void sortColors(int[] nums) {
-	        int lt = -1;//[0....lt]是0
-	        int gt = nums.length;//[gt，，，nums.length-1]是2
-	        int i = 0;//[lt+1,....i)是1
-	        
-	        //当i》=gt时候循环结束
-	        while(i < gt) {
-	        	if (nums[i]==0) {
-					swap(nums, i, lt+1);
+
+		public void sortColors(int[] nums) {
+			int lt = -1;// [0....lt]是0
+			int gt = nums.length;// [gt，，，nums.length-1]是2
+			int i = 0;// [lt+1,....i)是1
+
+			// 当i》=gt时候循环结束
+			while (i < gt) {
+				if (nums[i] == 0) {
+					swap(nums, i, lt + 1);
 					lt++;
 					i++;
-				}else if (nums[i] == 1) {
+				} else if (nums[i] == 1) {
 					i++;
-				}else {
-					swap(nums, i, gt-1);
+				} else {
+					swap(nums, i, gt - 1);
 					gt--;
 				}
-	        }
-	    }
+			}
+		}
 	}
 }
+
